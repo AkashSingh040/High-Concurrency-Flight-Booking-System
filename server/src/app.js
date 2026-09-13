4ed0364f1c13080bb346aa86cb0277a35cac650b
akash <ak96908266@gmail.com>
2026-09-13 14:28:56 +0530

    feat: add centralized error handler

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const flighroutes=require("./routes/flight.routes");
 const requestLogger=require("./middleware/requestLogger");
+const errorHandler = require("./middleware/errorHandler");
 
 const app=express();
 
@@ -9,10 +10,12 @@ app.use(requestLogger);
 
 app.use("/api/flights",flighroutes);
 
+
 app.get("/api/health",(req,res)=>{
     res.status(200).json({
         status:"ok"
     });
 });
+app.use(errorHandler);
 
 module.exports=app;
