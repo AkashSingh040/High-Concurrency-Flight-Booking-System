3f826a0ae7337b1399203cb914af4d59e8878b1e
akash <ak96908266@gmail.com>
2026-09-14 02:26:29 +0530

    feat: implement role based authorization

--- server/src/routes/flight.routes.js
+++ server/src/routes/flight.routes.js
@@ -1,15 +1,18 @@
 const express=require("express");
 const {getFlights,getFlightById,createFlight,updateFlight,patchFlight,deleteFlight}=require("../controllers/flight.controller");
+
 const validateFlight=require("../middleware/validateFlights");
+const authenticate = require("../middleware/auth.middleware");
+const authorize = require("../middleware/role.middleware");
 
 const router=express.Router();
 
-router.get("/",getFlights);
-router.get("/:id",getFlightById);
+router.get("/",authenticate,getFlights);
+router.get("/:id",authenticate,getFlightById);
 
-router.post("/",validateFlight,createFlight);
+router.post("/",authenticate,authorize("ADMIN"),validateFlight,createFlight);
 
-router.put("/:id", updateFlight);
-router.patch("/:id", patchFlight);
-router.delete("/:id", deleteFlight);
+router.put("/:id",authenticate,authorize("ADMIN"), updateFlight);
+router.patch("/:id", authenticate,authorize("ADMIN"),patchFlight);
+router.delete("/:id", authenticate,authorize("ADMIN"),deleteFlight);
 module.exports=router;
