5797737cc0ed0813a685d7b0d153caf2c90413ef
akash <ak96908266@gmail.com>
2026-09-14 02:30:56 +0530

    feat: setup cors

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,13 @@ const authRoutes=require("./routes/auth.routes")
 const requestLogger=require("./middleware/requestLogger");
 const errorHandler = require("./middleware/errorHandler");
 
+const cors=require("cors");
+
 const app=express();
+
+app.use(cors({
+    origin:process.env.FRONTEND_API
+}))
 
 app.use(express.json());
 app.use(requestLogger);
