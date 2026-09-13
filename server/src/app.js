ce9ccbedacf5fbcaceba9887519d63ec0326ec81
akash <ak96908266@gmail.com>
2026-09-13 14:23:23 +0530

    feat:add requestLogger middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express=require("express");
 const flighroutes=require("./routes/flight.routes");
+const requestLogger=require("./middleware/requestLogger");
 
 const app=express();
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.use("/api/flights",flighroutes);
 
